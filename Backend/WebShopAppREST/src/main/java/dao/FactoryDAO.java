026fb08946ace120f6d824f051a05ff15f242d81
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2024-06-04 04:08:59 +0200

    factory dao added

--- Backend/WebShopAppREST/src/main/java/dao/FactoryDAO.java
+++ Backend/WebShopAppREST/src/main/java/dao/FactoryDAO.java
@@ -1,22 +1,46 @@
 package dao;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import beans.Factory;
+import beans.Location;
 
 public class FactoryDAO {
 
-	private List<Factory> factoryList=new ArrayList<Factory>();
-	private String contextPath;
-	public FactoryDAO() {loadLFactoriesFromFile();}
-	
-	public Collection<Factory> GetAll(){
-		return factoryList;
-	}
-	private void loadLFactoriesFromFile() {
-		
+	private List<Factory> factoryList = new ArrayList<>();
+    private String contextPath;
+
+    public FactoryDAO() {}
+    public FactoryDAO(String contextPath) {
+        this.contextPath = contextPath;
+        loadFactoriesFromFile();
+    }
+
+    public List<Factory> getAll() {
+        return factoryList;
+    }
+
+    private void loadFactoriesFromFile() {
+        try (Reader reader = new FileReader(new File(contextPath, "resources/factories.json"))) {
+            Gson gson = new Gson();
+            List<Factory> loadedLocations = gson.fromJson(reader, new TypeToken<List<Factory>>() {}.getType());
+
+            if (loadedLocations != null) {
+            	factoryList.clear(); // Clear existing locations
+            	factoryList.addAll(loadedLocations);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error loading factories from file: " + e.getMessage());
+        }
     }
 }
